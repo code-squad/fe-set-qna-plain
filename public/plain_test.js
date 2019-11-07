e931fca05c3f773647714fdc10fa739fb0859939
starr9622 <sbseodev@gmail.com>
2019-11-07 23:13:43 +0900

    계정변경확인

--- public/plain_test.js
+++ public/plain_test.js
@@ -1,37 +1,36 @@
-
 //Plain 구현
 const Plain = (() => {
   //구현이 필요해....
   let date, comp, eff;
-  function action(){
-    temp = comp(); 
+  function action() {
+    temp = comp();
     temp.render();
-    if(eff) eff();
+    if (eff) eff();
     return temp;
   }
   return {
     renderComponent(Component) {
-        comp = Component;
-        return action();
+      comp = Component;
+      return action();
     },
     useState(_initVal) {
-      if(!date) date = _initVal;
-      function setDate(fn){
+      if (!date) date = _initVal;
+      function setDate(fn) {
         date = fn();
         action();
       }
       return [date, setDate];
     },
-    useEffect(fn){
+    useEffect(fn) {
       eff = fn;
-    },
-  }
+    }
+  };
 })();
 
 function plain_test() {
   const [foo, setFoo] = Plain.useState(``);
   const fireEvent = () => {
-    setFoo(value => (new Date).toLocaleTimeString() || value);
+    setFoo(value => new Date().toLocaleTimeString() || value);
   };
 
   Plain.useEffect(() => {
@@ -55,10 +54,12 @@ function plain_test() {
 let pd = Plain.renderComponent(plain_test);
 
 (async function loop() {
-  for(let i=0; i<5; i++) {
-    await new Promise( (resolve) => setTimeout( ()=> {
-      pd.fakeEvent();
-    resolve()
-    }, 1000));
+  for (let i = 0; i < 5; i++) {
+    await new Promise(resolve =>
+      setTimeout(() => {
+        pd.fakeEvent();
+        resolve();
+      }, 1000)
+    );
   }
 })();
