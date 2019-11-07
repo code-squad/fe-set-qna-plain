3842286630f695ec250ce0c159690ae383d11336
jjori-master <jjori.master@gmail.com>
2019-11-07 23:39:01 +0900

    미션 1 수행중. 어렵다..

--- public/plain_test.js
+++ public/plain_test.js
@@ -1,12 +1,39 @@
 
 //Plain 구현
 const Plain = (() => {
-  //구현이 필요해....
-t 
+  let component = null;
+  let runEffect = null;
+
+  return {
+    renderComponent: (plain_test) => {
+      component = plain_test();
+      return component;
+    },
+
+    useState: (value) => {
+      value = [value];
+
+      const setFoo = (fn) => {
+        console.log(value);
+        console.log(fn(value));
+        component.render();
+        runEffect();
+        value = [fn(value)];
+      }
+
+      return [value, setFoo];
+    },
+
+    useEffect: (fn) => {
+      runEffect = fn;
+    }
+  }
 })();
 
 function plain_test() {
   const [foo, setFoo] = Plain.useState(``);
+
+  debugger;
 
   const fireEvent = () => {
     setFoo(value => (new Date).toLocaleTimeString() || value);
@@ -18,7 +45,7 @@ function plain_test() {
   });
 
   return {
-    render() {
+    render() {      
       console.log(`[render] : ${foo}`);
     },
     fakeEvent() {
@@ -33,10 +60,10 @@ function plain_test() {
 let pd = Plain.renderComponent(plain_test);
 
 (async function loop() {
-  for(let i=0; i<5; i++) {
-    await new Promise( (resolve) => setTimeout( ()=> {
+  for (let i = 0; i < 5; i++) {
+    await new Promise((resolve) => setTimeout(() => {
       pd.fakeEvent();
-    resolve()
+      resolve()
     }, 1000));
   }
 })();
